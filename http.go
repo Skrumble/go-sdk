d2baaed641a4d442d0cd6b88ee2a72dd0e4b5e0b
Michael Dabydeen <michael.dabydeen@tulip.com>
2019-03-26 15:16:01 -0400

    Initial commit

---
+++ http.go
@@ -0,0 +1,26 @@
+package skrumble 
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const Version = "3.0"
+const userAgent = "skrumble-go/" + Version
+
+// Base URL 
+var BaseURL = "https://api.skrumble.com"
+
+// @TODO: implement error logic & structure 
+type skrumbleError struct {
+	Code 		int		`json: "code"`
+	Message 	string	`json:"message"`
+	MoreInfo 	string	`json:"more_info"`
+	Status 		int 	`json: "status"`
+}
+
+func newClient(clientName string) *Client {
+	// @TODO: implement new client logic here 
+}
